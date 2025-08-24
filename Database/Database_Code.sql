fbda2b6cbb306499cff02a9eb012259b23c3f4aa
Mark-ic <markodragojevicc@gmail.com>
2025-08-24 20:33:34 +0200

    Database 1.0

--- Database/Database_Code.sql
+++ Database/Database_Code.sql
@@ -1,106 +1,148 @@
--- Schema
-DROP SCHEMA IF EXISTS `FoodieExpress`;
-CREATE SCHEMA IF NOT EXISTS `FoodieExpress` DEFAULT CHARACTER SET utf8;
-USE `FoodieExpress`;
+-- MySQL Script corrected
+-- Schema FoodieExpress
 
--- Jelo
-CREATE TABLE IF NOT EXISTS `Jelo` (
+DROP SCHEMA IF EXISTS `FoodieExpress` ;
+CREATE SCHEMA IF NOT EXISTS `FoodieExpress` DEFAULT CHARACTER SET utf8 ;
+USE `FoodieExpress` ;
+
+-- -----------------------------------------------------
+-- Table Jelo
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Jelo` ;
+
+CREATE TABLE `Jelo` (
   `idJelo` INT NOT NULL AUTO_INCREMENT,
   `nazivJela` VARCHAR(45) NOT NULL,
-  `cena` INT NOT NULL CHECK (`cena` > 0),
+  `cena` INT NOT NULL,
   `slika` VARCHAR(100) NOT NULL,
   `vremePripreme` INT NOT NULL,
-  `brojPorudzbina` INT NOT NULL DEFAULT 0,
+  `brojPorudzbina` INT NOT NULL,
   PRIMARY KEY (`idJelo`),
   UNIQUE INDEX `naziv_UNIQUE` (`nazivJela` ASC)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
--- Sastojak
-CREATE TABLE IF NOT EXISTS `Sastojak` (
+-- -----------------------------------------------------
+-- Table Sastojak
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Sastojak` ;
+
+CREATE TABLE `Sastojak` (
   `idSastojak` INT NOT NULL AUTO_INCREMENT,
   `nazivSastojka` VARCHAR(45) NOT NULL,
   `alergen` VARCHAR(3) NULL,
   PRIMARY KEY (`idSastojak`)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
--- Jelo_Sastojak
-CREATE TABLE IF NOT EXISTS `Jelo_Sastojak` (
+-- -----------------------------------------------------
+-- Table Jelo_Sastojak
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Jelo_Sastojak` ;
+
+CREATE TABLE `Jelo_Sastojak` (
   `idJelo` INT NOT NULL,
   `idSastojak` INT NOT NULL,
   PRIMARY KEY (`idJelo`, `idSastojak`),
-  CONSTRAINT `fk_js_jelo`
+  CONSTRAINT `fk_jelo_sastojak_jelo`
     FOREIGN KEY (`idJelo`)
     REFERENCES `Jelo` (`idJelo`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_js_sastojak`
+  CONSTRAINT `fk_jelo_sastojak_sastojak`
     FOREIGN KEY (`idSastojak`)
     REFERENCES `Sastojak` (`idSastojak`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
--- Meni
-CREATE TABLE IF NOT EXISTS `Meni` (
+-- -----------------------------------------------------
+-- Table Meni
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Meni` ;
+
+CREATE TABLE `Meni` (
   `idMeni` INT NOT NULL AUTO_INCREMENT,
-  `dnevniMeni` TINYINT NOT NULL,
+  `dnevniMeni` varchar(5) NOT NULL,
+  `nazivMenija` VARCHAR(45) NULL,
   PRIMARY KEY (`idMeni`)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
--- Jelo_Meni
-CREATE TABLE IF NOT EXISTS `Jelo_Meni` (
+-- -----------------------------------------------------
+-- Table Jelo_Meni
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Jelo_Meni` ;
+
+CREATE TABLE `Jelo_Meni` (
   `idJelo` INT NOT NULL,
   `idMeni` INT NOT NULL,
   PRIMARY KEY (`idMeni`, `idJelo`),
-  CONSTRAINT `fk_jm_jelo`
+  CONSTRAINT `fk_jelomeni_jelo`
     FOREIGN KEY (`idJelo`)
     REFERENCES `Jelo` (`idJelo`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_jm_meni`
+  CONSTRAINT `fk_jelomeni_meni`
     FOREIGN KEY (`idMeni`)
     REFERENCES `Meni` (`idMeni`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
--- Korisnik
-CREATE TABLE IF NOT EXISTS `Korisnik` (
+-- -----------------------------------------------------
+-- Table Korisnik
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Korisnik` ;
+
+CREATE TABLE `Korisnik` (
   `idKorisnik` INT NOT NULL AUTO_INCREMENT,
   `ime` VARCHAR(45) NOT NULL,
   `prezime` VARCHAR(60) NOT NULL,
   `telefon` VARCHAR(45) NOT NULL,
   `uloga` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idKorisnik`)
-) ENGINE=InnoDB;
+) ENGINE = InnoDB;
 
--- Porudzbina
-CREATE TABLE IF NOT EXISTS `Porudzbina` (
+-- -----------------------------------------------------
+-- Table Porudzbina
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `Porudzbina` ;
+
+CREATE TABLE `Porudzbina` (
   `idPorudzbina` INT NOT NULL AUTO_INCREMENT,
   `preostaloVreme` INT NOT NULL,
   `status` VARCHAR(45) NOT NULL,
-  `nacinIsporuke` VARCHAR(45) NOT NULL,
+  `nacunIsporuke` VARCHAR(45) NOT NULL,
   `adresa` VARCHAR(85) NULL,
-  `idJelo` INT NOT NULL,
-  `idMeni` INT NOT NULL,
+  `idJelo` INT NULL,
+  `idMeni` INT NULL,
   `idKorisnik` INT NOT NULL,
   PRIMARY KEY (`idPorudzbina`),
-  INDEX `idJelo_idx` (`idJelo` ASC),
-  INDEX `idMeni_idx` (`idMeni` ASC),
-  INDEX `idKorisnik_idx` (`idKorisnik` ASC),
-  CONSTRAINT `fk_p_jelo`
+  CONSTRAINT `fk_porudzbina_jelo`
     FOREIGN KEY (`idJelo`)
     REFERENCES `Jelo` (`idJelo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_p_meni`
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_porudzbina_meni`
     FOREIGN KEY (`idMeni`)
     REFERENCES `Meni` (`idMeni`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_p_korisnik`
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_porudzbina_korisnik`
     FOREIGN KEY (`idKorisnik`)
     REFERENCES `Korisnik` (`idKorisnik`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE=InnoDB;
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE = InnoDB;
+
+DELIMITER $$
+
+CREATE TRIGGER povecaj_broj_porudzbina
+AFTER INSERT ON Porudzbina
+FOR EACH ROW
+BEGIN
+    UPDATE Jelo
+    SET brojPorudzbina = brojPorudzbina + 1
+    WHERE idJelo = NEW.idJelo;
+END$$
+
+DELIMITER ;
+
